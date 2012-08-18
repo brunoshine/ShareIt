c5535c323b16ed0f6663e6bb00602dec3d04bff0
Bruno Figueiredo <github@brunofigueiredo.com>
2012-08-18 23:34:31 +0100

    fixed absolute url generation

---
+++ ShareIt/Core/Extensions/UrlHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShareIt.Core.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ToPublicUrl(this UrlHelper urlHelper)
+        {
+            var httpContext = urlHelper.RequestContext.HttpContext;
+            var uriBuilder = new UriBuilder
+            {
+                Host = httpContext.Request.Url.Host,
+                Path = "/",
+                Port = 80,
+                Scheme = "http",
+            };
+
+            if (httpContext.Request.IsLocal)
+            {
+                uriBuilder.Port = httpContext.Request.Url.Port;
+            }
+            return uriBuilder.Uri.AbsoluteUri;
+        }
+    }
+}
